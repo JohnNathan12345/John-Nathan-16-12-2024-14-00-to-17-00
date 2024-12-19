ba24f106b3d3d13952ea43f1a9e194d6c61c77d5
JohnNathan12345 <yujoh8269z@sjijunior.edu.sg>
2024-12-19 07:16:39 +0000

    Python + Minecraft adventures!!!!!!!!!!!!

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -1,4 +1,13 @@
 # Write all your codes for Day 4 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day4")
+# counter = 0 
+
+# while counter < 9:
+#     print(counter)
+#     counter = counter + 1
+#  GAME TIME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! RIDDLES
+riddleAns = "Navigator"
+userAns  = input("What do alligators use to travel around the world? ")
+while userAns != riddleAns:
+    userAns  = input("WRONG ANSWER !!!!!!!!!!!!! What do alligators use to travel around the world? (Hint: Another word for GPS) ")
